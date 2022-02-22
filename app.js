eebaa103c61144b4c6a614543a8a8b52c076a053
Dyego Henrique <88366871+zebodexx@users.noreply.github.com>
2022-02-22 20:56:23 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/singup.html");
+});
+
+app.post("/", function(req, res){
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const emailUser = req.body.emailUser;
+    const data = {
+        members:[{
+            email_address: emailUser,
+            status: "subscribed",
+            merge_fields:{
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }]
+    };
+    
+    const jsonData = JSON.stringify(data);
+    const url = "https://us5.api.mailchimp.com/3.0/lists/5392cfb031";
+    const options = {
+        method: "POST",
+        auth: "zebode088:50a0d3bfe07e3502bc3990766ad67a9f-us5"
+    }
+
+    const request = https.request(url, options, function(response){
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/sucess.html");
+        }
+        else{
+            res.sendFile(__dirname + "/failure.html");  
+        }
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
+});
+
+    app.listen(process.env.PORT || 3000, function(){
+    console.log("PORTA 3000.");
+});
+
+// 50a0d3bfe07e3502bc3990766ad67a9f-us5 APIKEY
+// 5392cfb031 UNIC ID AUDIENC
